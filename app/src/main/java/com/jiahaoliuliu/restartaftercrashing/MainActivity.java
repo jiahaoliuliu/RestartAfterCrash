e6262346737ce99dd748aa1b6d80dca3137b1a00
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-11-03 11:16:04 +0400

    Created the button which crashes the app

--- app/src/main/java/com/jiahaoliuliu/restartaftercrashing/MainActivity.java
+++ app/src/main/java/com/jiahaoliuliu/restartaftercrashing/MainActivity.java
@@ -2,12 +2,24 @@ package com.jiahaoliuliu.restartaftercrashing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String crashThisApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Method used to crash the app
+     * @param view
+     */
+    public void crash(View view) {
+        // Launch null pointer exception
+        crashThisApp.charAt(0);
     }
 }
