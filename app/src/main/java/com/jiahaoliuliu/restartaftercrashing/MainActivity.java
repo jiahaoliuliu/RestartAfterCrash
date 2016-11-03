df6e449b54c5bf03dd63f84747333d272055dff9
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-11-03 11:13:03 +0400

    Created the first version of the app

---
+++ app/src/main/java/com/jiahaoliuliu/restartaftercrashing/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jiahaoliuliu.restartaftercrashing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
