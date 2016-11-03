142bd11aa3efbd1b4edf9601b46a24417444c4c3
Jiahao Liu <jiahaoliuliu@gmail.com>
2016-11-03 11:56:26 +0400

    Simplified the method to caught the exception

--- app/src/main/java/com/jiahaoliuliu/restartaftercrashing/MainApplication.java
+++ app/src/main/java/com/jiahaoliuliu/restartaftercrashing/MainApplication.java
@@ -19,41 +19,36 @@ public class MainApplication extends Application {
     public static final String INTENT_KEY_UNCAUGHT_EXCEPTION = "UncaughtException";
     public static final String INTENT_KEY_STACK_TRACE = "StackTrace";
 
+    private Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Thread.setDefaultUncaughtExceptionHandler(new MyExceptionHandler(this,
-                MainActivity.class));
+        // Init the variables
+        mContext = this;
+
+        Thread.setDefaultUncaughtExceptionHandler(mCaughtExceptionHandler);
     }
 
-    public class MyExceptionHandler implements
-            java.lang.Thread.UncaughtExceptionHandler {
-        private final Context myContext;
-        private final Class<?> myActivityClass;
-
-        public MyExceptionHandler(Context context, Class<?> c) {
-
-            myContext = context;
-            myActivityClass = c;
-        }
-
+    private Thread.UncaughtExceptionHandler mCaughtExceptionHandler = new Thread.UncaughtExceptionHandler() {
+        @Override
         public void uncaughtException(Thread thread, Throwable exception) {
 
             StringWriter stackTrace = new StringWriter();
             exception.printStackTrace(new PrintWriter(stackTrace));
             System.err.println(stackTrace);// You can use LogCat too
-            Intent intent = new Intent(myContext, myActivityClass);
+            Intent intent = new Intent(mContext, MainActivity.class);
             String s = stackTrace.toString();
             //you can use this String to know what caused the exception and in which Activity
             intent.putExtra(INTENT_KEY_UNCAUGHT_EXCEPTION,
                     "Exception is: " + stackTrace.toString());
             intent.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK );
             intent.putExtra(INTENT_KEY_STACK_TRACE, s);
-            myContext.startActivity(intent);
+            mContext.startActivity(intent);
             //for restarting the Activity
             Process.killProcess(Process.myPid());
             System.exit(0);
         }
-    }
+    };
 }
